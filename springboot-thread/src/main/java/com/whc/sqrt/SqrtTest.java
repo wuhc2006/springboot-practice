5266dff08a5076c30d577d75d08b813dc02e1505
whc <wuhc2006@qq.com>
2019-07-08 19:36:28 +0800

    用二分法求任意一个数(>1)的平方根

---
+++ springboot-thread/src/main/java/com/whc/sqrt/SqrtTest.java
@@ -0,0 +1,43 @@
+package com.whc.sqrt;
+
+/**
+ * 不用数学库，求出任何一个大于1的数的平方根
+ *
+ * @Author Administrator
+ * @Date 2019/7/7 20:46
+ */
+public class SqrtTest {
+
+    public static double sqrt(double value) {
+        if (value < 1) {
+            throw new IllegalArgumentException("参数不能小于1");
+        } else if (value == 0) {
+            return 0;
+        } else {
+            double low = 0;
+            double high = value;
+            double middle = (low + high) / 2;
+            double tmp = middle * middle;
+            int i = 0;
+            while (high - low > 0.00000000001) {
+                i++;
+                if (tmp < value) {
+                    low = middle;
+                } else if (tmp > value) {
+                    high = middle;
+                } else {
+                    return middle;
+                }
+                middle = (low + high) / 2;
+                tmp = middle * middle;
+            }
+            System.out.println("遍历了" + i + "次找到答案!");
+            return middle;
+        }
+    }
+
+    public static void main(String[] args) {
+        double value = 134635;
+        System.out.println(value + "的平方根为:" + sqrt(value));
+    }
+}
