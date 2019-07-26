cf15c2570d99b97a1b18976a536aac02c5413f39
wuhc2006 <wuhc2006@163.com>
2019-07-26 12:24:46 +0800

    修改api

--- springboot-shiro/src/main/java/com/whc/controller/UserController.java
+++ springboot-shiro/src/main/java/com/whc/controller/UserController.java
@@ -6,9 +6,7 @@ import com.whc.vo.ApiResponseVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -25,22 +23,22 @@ public class UserController{
     private UserService userService;
 
     @ApiOperation(value = "查找用户", tags = "查找用户")
-    @GetMapping("/selectOne")
-    public ApiResponseVO<Object> selectOne(Long id){
+    @GetMapping("/select/{id}")
+    public ApiResponseVO<Object> selectOne(@PathVariable Long id){
         return new ApiResponseVO<>(200, "查找成功!", userService.selectByPrimaryKey(id));
     }
 
     @ApiOperation(value = "新增用户", tags = "新增用户")
-    @GetMapping("/insertOne")
-    public ApiResponseVO<Object> insertOne(String username, String password){
+    @PostMapping("/insert")
+    public ApiResponseVO<Object> insertOne(@RequestParam String username,@RequestParam String password){
         User user = new User(username, password, new Date());
         this.userService.insertOne(user);
         return new ApiResponseVO<>(200, "新增成功!", user);
     }
 
     @ApiOperation(value = "修改用户", tags = "修改用户")
-    @GetMapping("/update")
-    public ApiResponseVO<Object> update(Long id, String username, String password){
+    @PostMapping("/update")
+    public ApiResponseVO<Object> update(@RequestParam Long id, @RequestParam String username,@RequestParam String password){
         User user = this.userService.selectByPrimaryKey(id);
         user.setUsername(username);
         user.setPassword(password);
@@ -50,8 +48,8 @@ public class UserController{
     }
 
     @ApiOperation(value = "删除用户", tags = "删除用户")
-    @GetMapping("/deleteById")
-    public ApiResponseVO<Object> deleteById(Long id){
+    @PostMapping("/delete/{id}")
+    public ApiResponseVO<Object> deleteById(@PathVariable Long id){
         this.userService.deleteById(id);
         return new ApiResponseVO<>(200, "删除成功！", id);
     }
